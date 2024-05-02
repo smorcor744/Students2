fd4620b6e72807ecb0278c0d2fb5cb3a621a5202
Sergio <mcsergio@iesrafaelalberti.es>
2024-05-02 02:50:22 +0200

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,175 @@
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.BottomCenter
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import java.io.File
+
+@Composable
+fun StudentList(students: SnapshotStateList<String>) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        LazyColumn(
+            modifier = Modifier.verticalScroll(rememberScrollState()).height(300.dp).width(150.dp)
+        ) {
+            items(students) { student ->
+                StudentItem(student = student, onDelete = { students.remove(student) })
+            }
+        }
+    }
+}
+
+@Composable
+fun StudentItem(student: String, onDelete: () -> Unit) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+    ) {
+        Text(text = student, modifier = Modifier.weight(1f))
+        IconButton(onClick = onDelete) {
+            Icon(Icons.Filled.Delete, contentDescription = "Delete student")
+        }
+    }
+}
+
+
+@Composable
+fun StudentAdd(students: SnapshotStateList<String>, newStudentName: MutableState<String>) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        TextField(
+            modifier = Modifier.background(Color.White),
+            value = newStudentName.value,
+            onValueChange = { newStudentName.value = it },
+            label = { Text("New student name") })
+        Button(onClick = { students.add(newStudentName.value); newStudentName.value = "" }) {
+            Text("Add new student")
+        }
+    }
+}
+
+@Composable
+fun SaveButton(students: SnapshotStateList<String>, file: File) {
+    Button(
+        onClick = {
+            file.bufferedWriter().use { out ->
+                students.forEach { out.write("$it\n") }
+            }
+        }
+    ) {
+        Text("Save changes")
+    }
+}
+
+
+@Composable
+fun DeleteButton(students: SnapshotStateList<String>) {
+    Button(
+        onClick = {
+            students.clear()
+        }
+    ) {
+        Text("Clear all")
+    }
+}
+
+@Composable
+fun Students(file: File): SnapshotStateList<String> {
+    val student = remember { mutableStateListOf<String>() }
+    file.forEachLine { linea ->
+        if (linea != "") {
+            linea.split(" ").map { student.add(it.trim()) }
+        }
+    }
+    return student
+}
+
+
+@Preview
+@Composable
+fun MainScreen() {
+    val file = File("Students.txt")
+    val students = Students(file)
+    val newStudentName = remember { mutableStateOf("") }
+    val number by remember { mutableStateOf(students.size) }
+
+
+    Box(
+        modifier = Modifier.fillMaxSize()
+            .background(Color.LightGray),
+        contentAlignment = Alignment.Center
+    ) {
+        Column {
+            Box(modifier = Modifier.fillMaxSize()) {
+                Column(
+                    modifier = Modifier
+                        .align(Alignment.CenterStart)
+                        .padding(start = 160.dp)
+
+                ) {
+                    StudentAdd(students, newStudentName)
+                }
+
+                Box(
+                    modifier = Modifier.fillMaxSize().padding(vertical = 20.dp),
+                    contentAlignment = BottomCenter
+                ) {
+                    SaveButton(students, file)
+                }
+
+            }
+
+        }
+
+        Column(
+            modifier = Modifier
+                .align(Alignment.CenterEnd)
+                .padding(end = 20.dp)
+
+            ) {
+            Spacer(modifier = Modifier.width(20.dp))
+
+            StudentText(number)
+            Box(
+                modifier = Modifier
+                    .padding(end = 160.dp)
+                    .background(Color.White)
+                    .border(width = 2.dp, color = Color.Black)
+            )
+            {
+                StudentList(students)
+            }
+            DeleteButton(students)
+        }
+
+
+    }
+}
+
+
+@Composable
+fun StudentText(number: Int) {
+    Text("Students: $number")
+}
+
+
+fun main() = application {
+    Window(onCloseRequest = ::exitApplication) {
+        MainScreen()
+    }
+}
+
+
---
+++ src/main/kotlin/Students.kt
@@ -0,0 +1,49 @@
+// import androidx.compose.foundation.background
+//import androidx.compose.foundation.layout.*
+//import androidx.compose.material.*
+//import androidx.compose.material.icons.Icons
+//import androidx.compose.material.icons.filled.Delete
+//import androidx.compose.runtime.*
+//import androidx.compose.ui.Alignment
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.unit.dp
+//import androidx.compose.material.Text
+//import androidx.compose.ui.graphics.Color
+//
+//@Composable
+//fun StudentList(students: MutableList<String>) {
+//    Spacer(modifier = Modifier.height(16.dp))
+//    students.forEachIndexed { index, student ->
+//        Row(verticalAlignment = Alignment.CenterVertically) {
+//            Text(text = student)
+//            IconButton(onClick = { students.removeAt(index) }) {
+//                Icon(Icons.Filled.Delete, contentDescription = "Delete student")
+//            }
+//        }
+//    }
+//
+//}
+//
+//
+//@Composable
+//fun StudentAdd(students: MutableList<String>, newStudentName: MutableState<String>) {
+//    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+//        TextField(
+//            modifier = Modifier.background(Color.White),
+//            value = newStudentName.value,
+//            onValueChange = { newStudentName.value = it },
+//            label = { Text("New student name") })
+//        Button(onClick = { students.add(newStudentName.value); newStudentName.value = "" }) {
+//            Text("Add new student")
+//        }
+//    }
+//}
+//
+//@Composable
+//fun SaveButton() {
+//    Button(
+//        onClick = { /* Aquí puedes implementar la lógica para guardar los cambios */ }
+//    ) {
+//        Text("Save changes")
+//    }
+//}
